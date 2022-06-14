2edd304332278a7dbe3ff9d5e92e88b0d3e13b56
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-14 21:59:00 +0700

    Updates

---
+++ precache-manifest.f088142c653b8fe782f41ffaaddb5139.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dadbf1176d481951210",
+    "url": "/socialite/static/css/main.0edfd575.chunk.css"
+  },
+  {
+    "revision": "6dadbf1176d481951210",
+    "url": "/socialite/static/js/main.6dadbf11.chunk.js"
+  },
+  {
+    "revision": "e5d1cd4fec6f9750df8d",
+    "url": "/socialite/static/css/1.4eabd856.chunk.css"
+  },
+  {
+    "revision": "e5d1cd4fec6f9750df8d",
+    "url": "/socialite/static/js/1.e5d1cd4f.chunk.js"
+  },
+  {
+    "revision": "cc5f2b7deb7863fce311",
+    "url": "/socialite/static/js/runtime~main.cc5f2b7d.js"
+  },
+  {
+    "revision": "7e39e69a11b3827c08d87aefdf040226",
+    "url": "/socialite/static/media/first-carousel.7e39e69a.jpg"
+  },
+  {
+    "revision": "d5bd0d710cdebdb53c9cdb0a8164ed9b",
+    "url": "/socialite/static/media/second-carousel.d5bd0d71.jpg"
+  },
+  {
+    "revision": "fe87e9d36e7130cb5340c1d65d7ccf26",
+    "url": "/socialite/static/media/third-carousel.fe87e9d3.jpg"
+  },
+  {
+    "revision": "21f3a8086d871af09faf1ccd5ec6db09",
+    "url": "/socialite/index.html"
+  }
+];
