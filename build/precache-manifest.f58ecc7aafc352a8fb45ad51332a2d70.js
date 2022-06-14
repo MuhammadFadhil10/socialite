2dda011068345116cfc983d42f77ddbf50bb6570
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-14 23:19:42 +0700

    remove build folder from gitignore

---
+++ build/precache-manifest.f58ecc7aafc352a8fb45ad51332a2d70.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dadbf1176d481951210",
+    "url": "./static/css/main.0edfd575.chunk.css"
+  },
+  {
+    "revision": "6dadbf1176d481951210",
+    "url": "./static/js/main.6dadbf11.chunk.js"
+  },
+  {
+    "revision": "e5d1cd4fec6f9750df8d",
+    "url": "./static/css/1.4eabd856.chunk.css"
+  },
+  {
+    "revision": "e5d1cd4fec6f9750df8d",
+    "url": "./static/js/1.e5d1cd4f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7e39e69a11b3827c08d87aefdf040226",
+    "url": "./static/media/first-carousel.7e39e69a.jpg"
+  },
+  {
+    "revision": "d5bd0d710cdebdb53c9cdb0a8164ed9b",
+    "url": "./static/media/second-carousel.d5bd0d71.jpg"
+  },
+  {
+    "revision": "fe87e9d36e7130cb5340c1d65d7ccf26",
+    "url": "./static/media/third-carousel.fe87e9d3.jpg"
+  },
+  {
+    "revision": "8336d22ac4e6a0aa3043a0125a8fecf8",
+    "url": "./index.html"
+  }
+];
